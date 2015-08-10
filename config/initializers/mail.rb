d2626bc7f58a92af136275b01253c954f58c797c
Nithya Rao <bumblebeenr@gmail.com>
2015-08-10 11:23:27 -0500

    Inprogress testing mailgun rcvg email

---
+++ config/initializers/mail.rb
@@ -0,0 +1,13 @@
+ActionMailer::Base.smtp_settings = {
+  port:              587, 
+  address:           'smtp.mailgun.org',
+  user_name:         ENV['MAILGUN_SMTP_LOGIN'],
+  password:          ENV['MAILGUN_SMTP_PASSWORD'],
+  domain:            'appc8d7672e677e431aab42fe7fe2b4b204.mailgun.org'
+  authentication:    :plain,
+  content_type:      'text/html'
+}
+ActionMailer::Base.delivery_method = :smtp
+
+# Makes debugging *way* easier.
+ActionMailer::Base.raise_delivery_errors = true
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:show] 
+  resources :topics do
+      resources :bookmarks
+   end
   get 'welcome/index'
   get 'about' =>'welcome#about'
+  post :incoming, to: 'incoming#create'
   root to: 'welcome#index'
 end
