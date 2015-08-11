c19ab7f7f7e99c1bc2348cb0c7168f7b47247407
Nithya Rao <bumblebeenr@gmail.com>
2015-08-11 13:50:40 -0500

    Inprogress

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:show] 
   resources :topics do
-      resources :bookmarks
-   end
+      resources :bookmarks do
+        resources :likes, only: [:create, :destroy]
+      end
+   end 
+    
   get 'welcome/index'
   get 'about' =>'welcome#about'
   get 'myblocmarks' => 'topics#index'
